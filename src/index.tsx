1c5af29b34c5b820320d34c2ede97f2e1635afd1
Gabriel <gpsnts@protonmail.com>
2021-08-24 17:12:07 -0300

    Added new html template

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello, uord</div>
-    <h1>H1</h1>
-    <h2>H2</h2>
-    <h3>H3</h3>
+    <div className="flex flex-col p-6 items-center justify-center h-full">
+      <h1 className="text-green-400 font-extrabold text-7xl">Hello World!</h1>
+      <p className="tracking-widest text-4xl">
+        This is a functional component and a template!
+      </p>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
